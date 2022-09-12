b677b554c5248bf81b1ec0c6775843850659288e
Nikita Chubukov <NikitaCH@bk.ru>
2022-09-12 21:39:35 +0300

    Function Tax has been updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,18 @@
 
 val amount = 1_000_000 // Сумма перевода в копейках
-val typeOfBill = "VkPay" // Тип карты/счета
-var amountTransferOfCurrentMonth = 0 // Сумма предыдущих переводов в этом месяце
 
-val minimumFee = 3500 // Минимальная комиссия при переводе с карт Visa и Мир
-val feeForVisaAndMir = 0.0075 //Ставка при переводе с карт Visa и Мир
+val minimumFeeForVisaAndMir = 3500 // Минимальная комиссия при переводе с карт Visa и Мир
+val feeForVisaAndMir = 0.0075 // Ставка при переводе с карт Visa и Мир 0.75%
 val feeForMasterAndMaestroPercent =  0.006 // Процент для комиссии при переводе с карт Mastercard и Maestro
 val feeForMasterAndMaestroInt = 20 * 100 // Часть комисси при переводе с карт Mastercard и Maestro не зависящая от суммы перевода
 val minBoundOfPromo = 300 * 100
 val maxBoundOfPromo = 75000 * 100
 
 fun main() {
-
-    println("Комиссия за перевод $amount коп. составит: ${tax()} коп.")
-
+    println("Комиссия за перевод $amount коп. составит: ${tax("VkPay", amount = amount )} коп.")
 }
 
-fun tax(): Int  {
+fun tax(typeOfBill: String = "VkPay", amountTransferOfCurrentMonth: Int = 0, amount: Int ): Int  {
     val fee =  when (typeOfBill){
         "Mastercard", "Maestro" -> {
             if (amountTransferOfCurrentMonth <= maxBoundOfPromo && amountTransferOfCurrentMonth >= minBoundOfPromo) {
@@ -26,9 +22,9 @@ fun tax(): Int  {
             }
         }
         "Visa", "Мир" -> {
-            if (amount * feeForVisaAndMir >= minimumFee)
+            if (amount * feeForVisaAndMir >= minimumFeeForVisaAndMir)
                 amount * feeForVisaAndMir
-            else minimumFee
+            else minimumFeeForVisaAndMir
         }
         else -> {0}
     }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,233 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun main() {
+        //arrange
+        val amount = 1_000_000 // Сумма перевода в копейках
+
+        //act
+       val result = "Комиссия за перевод $amount коп. составит: ${tax(amount = amount)} коп."
+
+        //assert
+        assertEquals("Комиссия за перевод 1000000 коп. составит: 0 коп.", result)
+
+    }
+    @Test
+    fun tax_DefaultTypeOfBill() {
+
+        //arrange
+        val amount = 1_000_000 // Сумма перевода в копейках
+
+        //act
+        val result = tax(amount = amount)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun tax_RandomTypeOfBillCase() {
+
+        //arrange
+        val amount = 1_000_000 // Сумма перевода в копейках
+        val typeOfBill = "1111" // Тип карты/счета
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun tax_VkPayCase() {
+
+        //arrange
+        val amount = 1_000_000 // Сумма перевода в копейках
+        val typeOfBill = "VkPay" // Тип карты/счета
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+
+    @Test
+    fun tax_VisaCaseMinBoundary() {
+        //arrange
+        val amount = 4667_00 // Сумма перевода в копейках
+        val typeOfBill = "Visa" // Тип карты/счета
+
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun tax_VisaCaseOutOfMinBoundary() {
+        //arrange
+        val amount = 4668_00 // Сумма перевода в копейках
+        val typeOfBill = "Visa" // Тип карты/счета
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(3501, result)
+    }
+
+    @Test
+    fun tax_MirCaseMinBoundary() {
+        //arrange
+        val amount = 4666_00 // Сумма перевода в копейках
+        val typeOfBill = "Мир" // Тип карты/счета
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun tax_MirCaseOutOfMinBoundary() {
+        //arrange
+        val amount = 4668_00 // Сумма перевода в копейках
+        val typeOfBill = "Мир" // Тип карты/счета
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount)
+
+        //assert
+        assertEquals(3501, result)
+    }
+
+    @Test
+    fun tax_MastercardBelowMinBoundary() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Mastercard" // Тип карты/счета
+        val minBoundOfPromo = 300 * 100
+        val amountTransferOfCurrentMonth = minBoundOfPromo - 1 // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(2006, result)
+    }
+
+    @Test
+    fun tax_MastercardInPromoBoundariesMin() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Mastercard" // Тип карты/счета
+        val minBoundOfPromo = 300 * 100
+        val amountTransferOfCurrentMonth = minBoundOfPromo // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun tax_MastercardAboveMaxBoundary() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Mastercard" // Тип карты/счета
+        val maxBoundOfPromo = 75000 * 100
+        val amountTransferOfCurrentMonth = maxBoundOfPromo + 1 // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(2006, result)
+    }
+
+    @Test
+    fun tax_MastercardInPromoBoundariesMax() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Mastercard" // Тип карты/счета
+        val maxBoundOfPromo = 75000 * 100
+        val amountTransferOfCurrentMonth = maxBoundOfPromo // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun tax_MaestroBelowMinBoundary() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Maestro" // Тип карты/счета
+        val minBoundOfPromo = 300 * 100
+        val amountTransferOfCurrentMonth = minBoundOfPromo - 1 // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(2006, result)
+    }
+
+    @Test
+    fun tax_MaestroInPromoBoundariesMin() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Maestro" // Тип карты/счета
+        val minBoundOfPromo = 300 * 100
+        val amountTransferOfCurrentMonth = minBoundOfPromo // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun tax_MaestroAboveMaxBoundary() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Maestro" // Тип карты/счета
+        val maxBoundOfPromo = 75000 * 100
+        val amountTransferOfCurrentMonth = maxBoundOfPromo + 1 // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(2006, result)
+    }
+
+    @Test
+    fun tax_MaestroInPromoBoundariesMax() {
+        //arrange
+        val amount = 10_00 // Сумма перевода в копейках
+        val typeOfBill = "Maestro" // Тип карты/счета
+        val maxBoundOfPromo = 75000 * 100
+        val amountTransferOfCurrentMonth = maxBoundOfPromo // Сумма предыдущих переводов в этом месяце
+
+        //act
+        val result = tax(typeOfBill = typeOfBill, amount = amount, amountTransferOfCurrentMonth = amountTransferOfCurrentMonth)
+
+        //assert
+        assertEquals(0, result)
+    }
+}
